0f617e26613d06cf78b5040c56ca6ec85698361f
Brandon Sears <brandons123@live.ca>
2026-05-03 16:34:10 -0300

    more stuff

--- frontend/src/build-info.js
+++ frontend/src/build-info.js
@@ -1,5 +1,5 @@
 export const BUILD_INFO = {
-  "buildId": "20260503180109-02c84992fb29",
-  "commit": "02c84992fb29",
-  "builtAt": "2026-05-03T18:01:09.946Z"
+  "buildId": "20260503193336-528fdc9c5ea8",
+  "commit": "528fdc9c5ea8",
+  "builtAt": "2026-05-03T19:33:36.302Z"
 };
