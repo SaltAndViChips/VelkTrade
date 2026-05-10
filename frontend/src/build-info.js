eeb7811f64086cfe1572ef638ca974e1064bc4a7
Brandon Sears <brandons123@live.ca>
2026-05-10 16:07:17 -0300

    Updated Deploy

--- frontend/src/build-info.js
+++ frontend/src/build-info.js
@@ -1,5 +1,5 @@
 export const BUILD_INFO = {
-  "buildId": "20260510041857-d6e4494b5d85",
-  "commit": "d6e4494b5d85",
-  "builtAt": "2026-05-10T04:18:57.108Z"
+  "buildId": "20260510190708-e2d5fd56bdf6",
+  "commit": "e2d5fd56bdf6",
+  "builtAt": "2026-05-10T19:07:08.075Z"
 };
